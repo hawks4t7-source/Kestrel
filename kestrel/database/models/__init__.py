86a3d9c558668dd996b95475d7dfb56ec5934cc2
Kashee <hawks4t7@gmail.com>
2026-07-23 05:17:20 +0500

    feat(database): add ORM model registry

--- kestrel/database/models/__init__.py
+++ kestrel/database/models/__init__.py
@@ -4,6 +4,7 @@ Kestrel database ORM models.
 
 from kestrel.database.models.base import Base
 from kestrel.database.models.mixins import TimestampMixin
+
 from kestrel.database.models.project import Project
 from kestrel.database.models.target import Target
 from kestrel.database.models.asset import Asset
